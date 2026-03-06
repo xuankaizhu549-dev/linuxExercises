eb410c8a0bbcdea7d516128dad81a9966e7d4240
xzhu392 <xzhu392@wisc.edu>
2026-03-05 23:12:02 -0600

    finish five_dirs.sh

---
+++ five_dirs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p five
+
+for i in {1..5}
+do
+    mkdir -p five/dir$i
+
+    for j in {1..4}
+    do
+        > five/dir$i/file$j
+
+        for k in $(seq 1 $j)
+        do
+            echo $j >> five/dir$i/file$j
+        done
+
+    done
+done
